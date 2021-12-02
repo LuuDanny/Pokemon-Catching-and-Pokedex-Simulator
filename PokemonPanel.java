b947cea9f91fe3b39f1f4a654da5fa229b8b8e56
Danny Luu <luudanny@hawaii.edu>
2021-12-02 13:23:52 -1000

    In-progress

--- PokemonPanel.java
+++ PokemonPanel.java
@@ -1,6 +1,7 @@
 import java.awt.*;
 import java.awt.event.*;
 import javax.swing.*;
+import javax.swing.border.*;
 import java.util.*;
 /**
 *Main Content Panel: Pokemon.
@@ -10,13 +11,22 @@ import java.util.*;
 
 public class PokemonPanel extends JPanel {
    
+   
    /** Pokemon image. */
    ImageIcon pokemon = new ImageIcon("Images/Pokemon.png");
    /** Pokedex image. */
    ImageIcon pokedex = new ImageIcon("Images/Pokedex.png");
    /** Backpack image. */
    ImageIcon backpack = new ImageIcon("Images/Backpack.png");
+   /** Backpack image. */
+   ImageIcon charizard = new ImageIcon("Images/Charizard.png");
    
+   
+   /** Text area for new pokemon. */
+   private JTextArea textArea = new JTextArea(5, 50);
+
+   
+   JLabel picCharizard = new JLabel(charizard);
    
    /** Label. */
    private JLabel lPokemon = new JLabel("Pokemon");
@@ -35,6 +45,10 @@ public class PokemonPanel extends JPanel {
    private JButton bBackpack = new JButton(backpack);
    /** Button. */
    private JButton bSort = new JButton("Sort");
+   /** Button. */
+   private JButton bHunt = new JButton("Hunt");
+   /** Button. */
+   private JButton bCatch = new JButton("Catch");
    
    /** Choise. */
    private Choice sortChoise = new Choice();
@@ -48,7 +62,16 @@ public class PokemonPanel extends JPanel {
    private JPanel pokemonTop = new JPanel();
    /** Pokemon Sub Panel. */
    private JPanel pokemonBottom = new JPanel();
+   /** Pokemon Sub Panel. */
+   private JPanel pokemonContainer = new JPanel();
+   /** Pokemon Sub Panel. */
+   private JPanel pokemonContainerTop = new JPanel();
+   /** Pokemon Sub Panel. */
+   private JPanel pokemonContainerBottom = new JPanel();
+   /** Pokemon Sub Panel. */
+   private JPanel pokemonContainerBottomButton = new JPanel();
    
+      
    /** Card-Panel: Pokedex. */
    private JPanel cardPokedex = new JPanel();
       /** Pokedex Sub Panel. */
@@ -67,7 +90,7 @@ public class PokemonPanel extends JPanel {
    /**Bottom Panel: Buttons for card navigation. */
    private JPanel buttonPanel = new JPanel();
 
-      /** Background Color .*/
+   /** Background Color .*/
    private Color cBlue = new Color(142, 239, 255);
    /** Background Color .*/
    private Color cRed = new Color(255, 20, 0);
@@ -79,14 +102,30 @@ public class PokemonPanel extends JPanel {
       this.setLayout(new BorderLayout());
       this.setPreferredSize(new Dimension(700, 600));
       deckPanel.setLayout(new CardLayout());
+      
+      //Pokemon Card-Panel and its sub-panels
       cardPokemon.setLayout(new BorderLayout());
+      pokemonContainer.setLayout(new BoxLayout(pokemonContainer, BoxLayout.Y_AXIS));
+      pokemonContainerTop.setLayout(new BoxLayout(pokemonContainerTop, BoxLayout.X_AXIS));
+      pokemonContainerBottom.setLayout(new BoxLayout(pokemonContainerBottom, BoxLayout.X_AXIS));
+      pokemonContainerBottomButton.setLayout(new BoxLayout(pokemonContainerBottomButton, BoxLayout.Y_AXIS));
+      
+      //Pokedex Card-Panel and its sub-panels
       cardPokedex.setLayout(new BorderLayout());
+      
+      //Backpack Card-Panel and its sub-panels
       cardBackpack.setLayout(new BorderLayout());
       backpackTop.setLayout(new BoxLayout(backpackTop, BoxLayout.LINE_AXIS));
       
+      Border margin = new MatteBorder(10,0,0,0, cBlue);
+      Border blackline = BorderFactory.createLineBorder(Color.black, 2);
+      TitledBorder title = BorderFactory.createTitledBorder(blackline, "Gotta catch 'em all!");
+      title.setTitleFont(new Font("Arial", Font.ITALIC, 14));
+      
+      
       //Temporary so that we can see the borders of each panel
       pokemonTop.setBackground(cBlue);
-      pokemonBottom.setBackground(cRed);
+      pokemonBottom.setBackground(cBlue);
       pokedexTop.setBackground(cBlue);
       pokedexBottom.setBackground(cRed);
       backpackTop.setBackground(cBlue);
@@ -96,24 +135,47 @@ public class PokemonPanel extends JPanel {
 
       
       this.add("Center", deckPanel);
-      //Adding card panels to the deck"
+      //Adding CARD-PANELS (pokemon, pokedex, & backpack) to the DECK"
       deckPanel.add(cardPokemon, "pokemon");
       deckPanel.add(cardPokedex, "pokedex");
       deckPanel.add(cardBackpack, "backpack");
       
-      //Adding sub-panels to pokemon card
+      //========================== Pokemon Card Panel ==========================
       cardPokemon.add("North", pokemonTop);
       cardPokemon.add("Center", pokemonBottom);
       
       pokemonTop.add(lPokemon);
+      pokemonBottom.add(pokemonContainer);
       
-      //Adding sub-panels to pokedex card
+      pokemonContainer.setBorder(new CompoundBorder(margin, blackline));
+      pokemonContainer.setPreferredSize(new Dimension(600, 410));
+      
+      
+      picCharizard.setAlignmentX(picCharizard.CENTER_ALIGNMENT);
+      textArea.setAlignmentX(textArea.CENTER_ALIGNMENT);
+      textArea.setMaximumSize(textArea.getPreferredSize());
+      textArea.setBorder(title);
+      
+      pokemonContainer.add(picCharizard);
+      pokemonContainer.add(Box.createRigidArea(new Dimension(0,65)));
+      pokemonContainer.add(textArea);
+      textArea.setEditable(false);
+
+      
+      
+      
+      //========================== Pokedex Card-Panel ==========================
       cardPokedex.add("North", pokedexTop);
       cardPokedex.add("Center", pokedexBottom);
       
       pokedexTop.add(lPokedex);
       
-      //Adding sub-panels to backpack card
+      
+      
+      
+      
+      
+      //========================== Backpack Card-Panel ==========================
       cardBackpack.add("North", backpackTop);
       cardBackpack.add("Center", backpackBottom);
       
